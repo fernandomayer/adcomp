a5e75858b14287048534a52bd94127e603735d5e
Fernando Mayer <fernandomayer@gmail.com>
2018-08-24 18:16:39 -0300

    Understandin "predictions"

--- tmb_examples/thetalog2.R
+++ tmb_examples/thetalog2.R
@@ -37,8 +37,8 @@ thetalog <- function(Y, X, logr0, logtheta, logK, logQ, logR){
     Q <- exp(logQ)
     R <- exp(logR)
     timeSteps <- length(Y)
-    m <- numeric(length(timeSteps))
-    ans <- numeric(length(timeSteps))
+    m <- numeric(timeSteps)
+    ans <- numeric(timeSteps)
     for(i in 2:timeSteps){
         ## Note que o X é utilizado para o cálculo da verossimilhança,
         ## mas nesse caso, X[i-1] e X[i] serão sempre 0
@@ -135,6 +135,100 @@ plot(Y, ylim = ylim)
 lines(m0$X)
 lines(rep$par.random, col = 2)
 ## \o/
+
+##----------------------------------------------------------------------
+## Making a prediction
+## y_t = x_t + v_t, v_t ~ N(0, R)
+xt <- rep$par.random
+R <- exp(rep$par.fixed[5])
+vt <- rnorm(length(xt), 0, R)
+yhat <- xt + vt
+lines(yhat, col = 3)
+## It does not make much sense here.
+
+##----------------------------------------------------------------------
+## Same function, but without using log in the parameters, just to see
+## wheter it maks a difference or not
+thetalog2 <- function(Y, X, r0, theta, K, Q, R){
+    timeSteps <- length(Y)
+    m <- numeric(timeSteps)
+    ans <- numeric(timeSteps)
+    for(i in 2:timeSteps){
+        m[i-1] <- X[i-1] + r0*(1 - (exp(X[i-1])/K)^theta)
+        ans[i-1] <- dnorm(X[i], m[i-1], sqrt(Q), log = TRUE)
+    }
+    ans <- -sum(ans) # vero de X
+    ans2 <- numeric(length(timeSteps))
+    for(i in 1:timeSteps){
+        ans2[i] <- dnorm(Y[i], X[i], sqrt(R), log = TRUE)
+    }
+    ans2 <- -sum(ans2) # vero de Y
+    res <- ans + ans2  # vero conjunta
+    for(i in 2:timeSteps){
+        X[i] <- X[i-1] + r0*(1 - (exp(X[i-1])/K)^theta)
+    }
+    saida <- list(m = m, X = X, nllX = ans, nllY = ans2, nll = res)
+    return(saida)
+}
+
+## Aplica a função
+m1 <- thetalog2(Y = Y, X = numeric(length(Y)),
+               r0 = 1, theta = 1, K = exp(6),
+               Q = 1, R = 1)
+m1[3:5]
+
+## Portanto note que são iguais
+m0$nll
+m1$nll
+obj$fn(obj$par)
+## NO difference!
+
+##----------------------------------------------------------------------
+## Trying to optimize the function directly
+## Assuming there is no process erros, i.e., only observation likelihood
+## to be optimized
+fnthetalog <- function(par, Y){
+    r0 <- par[1]
+    theta <- par[2]
+    K <- par[3]
+    R <- par[4]
+    timeSteps <- length(Y)
+    X <- numeric(timeSteps)
+    ans <- numeric(timeSteps)
+    X[1] <- K
+    for(i in 2:timeSteps){
+        X[i] <- X[i-1] + r0*(1 - (exp(X[i-1])/K)^theta)
+    }
+    for(i in 1:timeSteps){
+        ans[i] <- dnorm(Y[i], X[i], sqrt(R), log = TRUE)
+    }
+    ans <- -sum(ans) # vero de Y
+    return(ans)
+}
+
+## Aplica a função
+fnthetalog(par = c(1, 1, 6, 1), Y = Y)
+
+## Optimze
+opt1 <- nlminb(start = c(1, 1, 6, 1), objective = fnthetalog, Y = Y,
+               lower = 0, upper = Inf)
+opt1
+opt1$par
+
+model <- function(X0, r0, theta, K, R, n){
+    X <- numeric(n)
+    X[1] <- K
+    for(i in 2:n){
+        X[i] <- X[i-1] + r0*(1 - (exp(X[i-1])/K)^theta)
+    }
+    return(X)
+}
+
+xpred <- model(X0 = opt1$par[3], r0 = opt1$par[1],
+               theta = opt1$par[2], K = opt1$par[3],
+               R = opt1$par[4], n = length(Y))
+
+lines(xpred, col = 4)
 
 ## VER:
 
